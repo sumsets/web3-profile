3f1ee0ae4966f90cc77023c54977aac1d3b152aa
ridvansumset <ridvan.sumset@coda.xyz>
2022-03-30 16:22:21 +0100

    wifi component baslangic

--- src/components/ProfileTop.vue
+++ src/components/ProfileTop.vue
@@ -1,46 +1,56 @@
 <template>
 <div id="head">
-<div id="status-bar">
-<div id="status-bar-left">{{time}}</div>
-<div id="status-bar-right">
-<div>{{mobil}}</div>
-<div>{{connect}}</div>
-<div>{{battery}}</div>
-</div>
-</div>
-<div id="back-more-bar">
-<div id="back-button">{{back}}</div>
-<div id="more-button">{{more}}</div>
-</div>
-<div id="wallet-token-bar">
-<div id="wallet">
-<div id="wallet-icon">{{dolar}}</div>
-<div id="director">{{name}}</div>
-</div>
-<div id="token">
-<div id="token-icon">{{euro}}</div>
-<div id="token-total">{{token}}</div>
-</div>
-</div>
-<div id="profile-section">
-<img :src="pp" alt="">
-<div id="user-id">{{user}}</div>
-<div id="badges">
-<img :src="badge1" alt="">
-<img :src="badge2" alt="">
-<img :src="badge3" alt="">
-</div>
-</div>
+  <div id="status-bar">
+    <span id="status-bar-left">
+      09:41
+    </span>
+
+    <div id="status-bar-right">
+      <!-- <img src="@/assets/mobil.png" /> -->
+
+      <profile-top-wifi :degree="wifiLevel"></profile-top-wifi>
+
+      <!-- <img src="@/assets/battery.png" alt="bateri ikonu" /> -->
+    </div>
+  </div>
+
+    <div id="back-more-bar">
+    <div id="back-button">{{back}}</div>
+    <div id="more-button">{{more}}</div>
+    </div>
+    <div id="wallet-token-bar">
+    <div id="wallet">
+    <div id="wallet-icon">{{dolar}}</div>
+    <div id="director">{{name}}</div>
+    </div>
+    <div id="token">
+    <div id="token-icon">{{euro}}</div>
+    <div id="token-total">{{token}}</div>
+    </div>
+    </div>
+    <div id="profile-section">
+    <img :src="pp" alt="">
+    <div id="user-id">{{user}}</div>
+    <div id="badges">
+    <img :src="badge1" alt="">
+    <img :src="badge2" alt="">
+    <img :src="badge3" alt="">
+    </div>
+    </div>
 </div>
 </template>
+
 <script>
+import ProfileTopWifi from './ProfileTopWifi.vue';
+
 export default {
+  components: {
+    ProfileTopWifi,
+  },
   data() {
     return {
-      time: '20:36',
       mobil: 'sinyal',
-      connect: 'wifi',
-      battery: 'full',
+      wifiLevel: 1,
       back: '../assets/Frame.svg',
       more: '././assets/Group 65.svg',
       dolar: '$',
---
+++ src/components/ProfileTopWifi.vue
@@ -0,0 +1,15 @@
+<template>
+  <div div v-if="degree === 0">{{ degree }}</div>
+  <div v-else>{{ degree }}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    degree: Number,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
