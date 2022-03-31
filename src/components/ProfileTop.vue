29feb85b1f0ed040bec0d0098769a98c7e405536
NumberZOne <sumsetoguzhan@gmail.com>
2022-03-31 22:31:18 +0300

    css düzenlemeleri

--- src/components/ProfileTop.vue
+++ src/components/ProfileTop.vue
@@ -1,47 +1,51 @@
 <template>
-  <div id="head" :style="{ backgroundImage: 'url(' + imageUrl + ')' }">
-    <div id="status-bar">
-      <span id="status-bar-left">
-        09:41
-      </span>
+  <div id="head">
+    <div id="upper-head" :style="{ backgroundImage: 'url(' + imageUrl + ')' }">
+      <div id="status-bar">
+        <span id="status-bar-left">
+          09:41
+        </span>
 
-      <div id="status-bar-right">
-        <img class="right-bar-icons" src="@/assets/MobileSignal.svg" alt="sinyal" />
+        <div id="status-bar-right">
+          <img class="right-bar-icons" src="@/assets/MobileSignal.svg" alt="sinyal" />
 
-        <profile-top-wifi class="right-bar-icons" :degree="wifiLevel"></profile-top-wifi>
+          <profile-top-wifi class="right-bar-icons" :degree="wifiLevel"></profile-top-wifi>
 
-        <img class="right-bar-icons" src="@/assets/Battery.svg" alt="bateri ikonu" />
+          <img class="right-bar-icons" src="@/assets/Battery.svg" alt="bateri ikonu" />
+        </div>
+      </div>
+
+      <div id="back-more-bar">
+        <img src="@/assets/Frame.svg" alt="back" />
+        <img src="@/assets/Group 65.svg" alt="more" />
+      </div>
+
+      <div id="wallet-token-bar">
+        <div id="wallet">
+          <img  id="wallet-icon" src="@/assets/wallet-icon.svg" alt="wallet-icon">
+          <span id="director">{{code}}</span>
+        </div>
+
+        <div id="token">
+          <img id="token-icon" src="@/assets/tokens.svg" alt="tokens" />
+          <span id="token-total">{{token}}</span>
+        </div>
       </div>
     </div>
 
-    <div id="back-more-bar">
-      <img src="@/assets/Frame.svg" alt="back" />
-      <img src="@/assets/Group 65.svg" alt="more" />
-    </div>
+    <div id="upper-head2" :style="{ backgroundImage: 'url(' + imageUrl2 + ')' }">
+      <div id="profile-section">
+        <img :src="pp" alt="">
 
-    <div id="wallet-token-bar">
-      <div id="wallet">
-        <img  id="wallet-icon" src="@/assets/wallet-icon.svg" alt="wallet-icon">
-        <span id="director">{{code}}</span>
-      </div>
+        <div id="user-id">
+          {{user}}
+        </div>
 
-      <div id="token">
-        <img id="token-icon" src="@/assets/tokens.svg" alt="tokens" />
-        <span id="token-total">{{token}}</span>
-      </div>
-    </div>
-
-    <div id="profile-section">
-      <img :src="pp" alt="">
-
-      <div id="user-id">
-        {{user}}
-      </div>
-
-      <div id="badges">
-        <img :src="badge1" alt="">
-        <img :src="badge2" alt="">
-        <img :src="badge3" alt="">
+        <div id="badges">
+          <img :src="badge1" alt="">
+          <img :src="badge2" alt="">
+          <img :src="badge3" alt="">
+        </div>
       </div>
     </div>
   </div>
@@ -57,11 +61,11 @@ export default {
   data() {
     return {
       mobil: 'sinyal',
-      wifiLevel: 2,
+      wifiLevel: 3,
       back: '../assets/Frame.svg',
       more: '././assets/Group 65.svg',
       code: 'Ox134...5bed',
-      token: '1001',
+      token: '420',
       pp: './assets/unsplash_6anudmplLw4.svg',
       user: 'Leroy Watson',
       badge1: './assets/Ranking 1.svg',
@@ -69,18 +73,28 @@ export default {
       badge3: './assets/Top Student.svg',
       // eslint-disable-next-line
       imageUrl: require('@/assets/top-background.svg'),
+      // eslint-disable-next-line
+      imageUrl2: require('@/assets/top2-background.svg'),
     };
   },
 };
 </script>
 
 <style scoped lang="scss">
+#head {
+  background-color: #E5E5E5;
+}
+#upper-head {
+  height: 154px;
+  position: relative;
+  z-index: 1;
+}
 #status-bar {
     color: white;
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
-    padding: 15px 24px;
+    padding: 13px 24px;
 }
 #status-bar-right{
     display: flex;
@@ -95,20 +109,22 @@ export default {
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
-    padding: 15px 24px;
+    padding: 10px 24px;
 }
 #wallet-token-bar{
     display: flex;
     justify-content: space-between;
-    padding: 15px 3px;
+    padding: 8px 3px;
     align-items: center;
+    margin-top: 20px;
+
 }
 #wallet{
     display: flex;
     border: 1px solid #BBD5DE;
-    border-radius: 10px;
+    border-radius: 8px;
     background-color: #BBD5DE;
-    padding: 9px;
+    padding: 4px 9px;
     margin-left: 15px;
     align-items: center;
 }
@@ -117,6 +133,7 @@ export default {
 }
 #director {
  text-decoration: underline;
+ font-size: 12px;
 }
 #token{
     display: flex;
